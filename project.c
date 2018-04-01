87ba6aa08e4216b2a1f91b7ac85417a123e4b10a
Ravishankar Singh <ravistormo@gmail.com>
2018-04-01 13:23:29 +0530

    separated input using functions

--- project.c
+++ project.c
@@ -2,61 +2,69 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int n,i;
 struct processs
 {
 	int burst_time;
 	int arrival_time;
 	int priority;
 };
+
+void burst_assign(struct processs *pp_array);
+void arrival_assign(struct processs *pp_array);
+void priority_assign(struct processs *pp_array);
+void display(struct processs *pp_array);
+
 int main()
 {
 	system("clear");
-
-	//data types
-	int n;
-	int i;
-	int flag=0;
-	struct processs *process_array;
-
-	//asking for number of processes
-	printf("\nNumber of Processes : ");
+	struct processs *p_array;
+	printf("\nNumber of Processes : ");									//asking for number of processes
 	scanf("%d",&n);
+	p_array=(struct processs*)calloc(n,sizeof(struct processs));		//allocating number of processes
+	burst_assign(p_array);												//asking BURST_TIME
+	arrival_assign(p_array);											//asking ARRIVAL_TIME
+	priority_assign(p_array);											//asking PRIORITY
+	display(p_array);													//displaying PROCESSES along with BURST TIME and ARRIVAL TIME and PRIORITY
 
-	//allocating number of processes
-	process_array=(struct processs*)calloc(n,sizeof(struct processs));
+}
 
-	//asking BURST_TIME
+void burst_assign(struct processs *pp_array)
+{
 	printf("\nEnter the BURST_TIME :\n\n\tPROCESS\t\tBURST_TIME\n");
 	for(i=0;i<n;i++)
 	{
 		printf("\n\t   P%d\t\t    ",i);
-		scanf("%d",&((process_array+i)->burst_time));
+		scanf("%d",&((pp_array+i)->burst_time));
 	}
-/********************************************************************
-	 //displaying process_array burst_time value
-	for(i=0;i<n;i++)
-	printf("%d\t",(process_array+i)->burst_time);
-*********************************************************************/
+}
 
-
-	//asking ARRIVAL_TIME
+void arrival_assign(struct processs *pp_array)
+{
 	system("clear");
 	printf("\nNumber of Processes : %d",n);
 	printf("\nEnter the ARRIVAL_TIME :\n\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\n");
 	for(i=0;i<n;i++)
 	{
-		printf("\n\t   P%d\t\t   %d\t\t   ",i,((process_array+i)->burst_time));
-		scanf("%d",&((process_array+i)->arrival_time));
+		printf("\n\t   P%d\t\t   %d\t\t   ",i,((pp_array+i)->burst_time));
+		scanf("%d",&((pp_array+i)->arrival_time));
 	}
-
-	//asking PRIORITY
+}
+void priority_assign(struct processs *pp_array)
+{
 	system("clear");
 	printf("\nNumber of Processes : %d",n);
 	printf("\nEnter the associated PRIORITY :\n\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\tPRIORITY\n");
 	for(i=0;i<n;i++)
 	{
-		printf("\n\t   P%d\t\t   %d\t\t   %d\t\t   ",i,((process_array+i)->burst_time),((process_array+i)->arrival_time));
-		scanf("%d",&((process_array+i)->priority));
+		printf("\n\t   P%d\t\t   %d\t\t   %d\t\t   ",i,((pp_array+i)->burst_time),((pp_array+i)->arrival_time));
+		scanf("%d",&((pp_array+i)->priority));
 	}
 }
-
+void display(struct processs *pp_array)
+{
+	printf("\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\tPRIORITY\n");
+	for(i=0;i<n;i++)
+	printf("\n\t  P%d\t\t   %d\t\t   %d\t\t   %d",i,(pp_array+i)->burst_time,(pp_array+i)->arrival_time,(pp_array+i)->priority);
+	printf("\n");
+}
