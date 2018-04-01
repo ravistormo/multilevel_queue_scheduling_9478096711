40544370f3d92fc61168df1e62bc19f37a77ac79
Ravishankar Singh <ravistormo@gmail.com>
2018-04-01 13:04:12 +0530

    changes- applying queue using structure

--- project.c
+++ project.c
@@ -1,38 +1,62 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
+
+struct processs
+{
+	int burst_time;
+	int arrival_time;
+	int priority;
+};
 int main()
 {
 	system("clear");
-	int i,n,*burst_array,*arrival_array,*priority_array;
+
+	//data types
+	int n;
+	int i;
+	int flag=0;
+	struct processs *process_array;
+
+	//asking for number of processes
 	printf("\nNumber of Processes : ");
 	scanf("%d",&n);
 
+	//allocating number of processes
+	process_array=(struct processs*)calloc(n,sizeof(struct processs));
+
+	//asking BURST_TIME
 	printf("\nEnter the BURST_TIME :\n\n\tPROCESS\t\tBURST_TIME\n");
-	burst_array=(int*)calloc(n,sizeof(int));
 	for(i=0;i<n;i++)
 	{
 		printf("\n\t   P%d\t\t    ",i);
-		scanf("%d",burst_array+i);
+		scanf("%d",&((process_array+i)->burst_time));
 	}
+/********************************************************************
+	 //displaying process_array burst_time value
+	for(i=0;i<n;i++)
+	printf("%d\t",(process_array+i)->burst_time);
+*********************************************************************/
 
+
+	//asking ARRIVAL_TIME
 	system("clear");
 	printf("\nNumber of Processes : %d",n);
 	printf("\nEnter the ARRIVAL_TIME :\n\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\n");
-	arrival_array=(int*)calloc(n,sizeof(int));
 	for(i=0;i<n;i++)
 	{
-		printf("\n\t   P%d\t\t    %d\t\t   ",i,*(burst_array+i));
-		scanf("%d",&(*(arrival_array+i)));
+		printf("\n\t   P%d\t\t   %d\t\t   ",i,((process_array+i)->burst_time));
+		scanf("%d",&((process_array+i)->arrival_time));
 	}
 
+	//asking PRIORITY
 	system("clear");
 	printf("\nNumber of Processes : %d",n);
-	printf("\nEnter the PRIORITY associated:\n\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\tPRIORITY\n");
-	priority_array=(int*)calloc(n,sizeof(int));
+	printf("\nEnter the associated PRIORITY :\n\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\tPRIORITY\n");
 	for(i=0;i<n;i++)
 	{
-		printf("\n\t   P%d\t\t   %d\t\t   %d\t\t   ",i,*(burst_array+i),*(arrival_array+i));
-		scanf("%d",&(*(priority_array+i)));
+		printf("\n\t   P%d\t\t   %d\t\t   %d\t\t   ",i,((process_array+i)->burst_time),((process_array+i)->arrival_time));
+		scanf("%d",&((process_array+i)->priority));
 	}
 }
+
