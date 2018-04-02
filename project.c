7bfc655e79f61be6ad7a754be68ded00e7c2f991
Ravishankar Singh <ravistormo@gmail.com>
2018-04-02 16:10:31 +0530

    added exception in input
    burst_time!<=0
    arrival_time!<0
    1<=priority<=10
    
    only integer input

--- project.c
+++ project.c
@@ -1,7 +1,6 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
-
 int n,i;
 int q1=0,q2=0,q3=0;
 struct processs
@@ -22,21 +21,21 @@ void burst_assign(struct processs *pp_array);		//function to assign burst time
 void arrival_assign(struct processs *pp_array);		//function to assign arrival time
 void priority_assign(struct processs *pp_array);	//function to assign priority
 void display(struct processs *pp_array);			//function to display all processes
-void display1(struct queuee *qq_array,int q);				//function to display queue wise processes
+void display1(struct queuee *qq_array,int q);		//function to display queue wise processes
 void queue_size(struct processs *pp_array);			//function to determine the size of each queue
 void queue_assign(struct processs *pp_array);		//function for assigning queue
 //***********************************************************************************************
 //**************************************MAIN*****************************************************
 int main()
 {
-	system("clear");	
+	system("cls");	
 
 	printf("\nNumber of Processes : ");scanf("%d",&n);				//asking for number of processes
 	p_array=(struct processs*)calloc(n,sizeof(struct processs));	//allocating number of processes
 	burst_assign(p_array);											//asking BURST_TIME
 	arrival_assign(p_array);										//asking ARRIVAL_TIME
 	priority_assign(p_array);										//asking PRIORITY
-	system("clear");
+	system("cls");
 	display(p_array);												//displaying PROCESSES along with BURST TIME and ARRIVAL TIME and PRIORITY
 	
 	queue_size(p_array);											//determining size of each queue q1,q2,q3
@@ -44,52 +43,72 @@ int main()
 	queue_two=(struct queuee*)calloc(q2,sizeof(struct queuee));		//allocating memory to queue_two
 	queue_three=(struct queuee*)calloc(q3,sizeof(struct queuee));	//allocating memory to queue_three
 	queue_assign(p_array);											//assigning processes to respective queue
-	printf("\nThe generated QUEUES are :\nQUEUE : 1\n\n");
+	printf("\nDividing processes on basis of PRIORITY.\n\nPRIORITY\t\tQUEUE\n [1,3]\t\t\t Q1\n [4,7]\t\t\t Q2\n[8, 10]\t\t\t Q3\n");
+	printf("\nThe generated QUEUES are :\nQUEUE 1 : (%d pocesses)\n",q1);
 	display1(queue_one,q1);
-	printf("\nQUEUE : 2\n\n");
+	printf("\n\nQUEUE 2 : (%d processes)\n",q2);
 	display1(queue_two,q2);
-	printf("\nQUEUE : 3\n\n");
+	printf("\n\nQUEUE 3 : (%d processes)\n",q3);
 	display1(queue_three,q3);
 
 }
 //*************************************************************************************************
 void burst_assign(struct processs *pp_array)
 {
+	printf("\n\t\t\t\t\tINPUT ONLY INTEGER VALUES !!!\n");
 	printf("\nEnter the BURST_TIME :\n\n\tPROCESS\t\tBURST_TIME\n");
 	for(i=0;i<n;i++)
 	{
-		printf("\n\t   P%d\t\t    ",i);
-		scanf("%d",&((pp_array+i)->burst_time));
+		do
+		{
+			printf("\t   P%d\t\t    ",i);
+			scanf("%d",&((pp_array+i)->burst_time));
+			if(((pp_array+i)->burst_time)<=0)
+			printf("\t\t\t\t\tBURST_TIME can not be <=0 !!Enter Again...\n");
+		}while(((pp_array+i)->burst_time)<=0);
 	}
 }
 //**************************************************************************************************
 void arrival_assign(struct processs *pp_array)
 {
-	system("clear");
+	system("cls");
 	printf("\nNumber of Processes : %d",n);
+	printf("\n\t\t\t\t\tINPUT ONLY INTEGER VALUES !!!\n");
 	printf("\nEnter the ARRIVAL_TIME :\n\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\n");
 	for(i=0;i<n;i++)
 	{
-		printf("\n\t   P%d\t\t   %d\t\t   ",i,((pp_array+i)->burst_time));
-		scanf("%d",&((pp_array+i)->arrival_time));
+		do
+		{
+			printf("\t   P%d\t\t   %d\t\t   ",i,((pp_array+i)->burst_time));
+			scanf("%d",&((pp_array+i)->arrival_time));
+			if(((pp_array+i)->arrival_time)<0)
+			printf("\t\t\t\t\t\tARRIVAL_TIME can not be <0 !!Enter Again...\n");
+		}while(((pp_array+i)->arrival_time)<0);
 	}
 }
 //**************************************************************************************************
 void priority_assign(struct processs *pp_array)
 {
-	system("clear");
+	system("cls");
 	printf("\nNumber of Processes : %d",n);
+	printf("\n\t\t\t\t\tINPUT ONLY INTEGER VALUES !!!\n");
 	printf("\nEnter the associated PRIORITY[1-10]:\n\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\tPRIORITY\n");
 	for(i=0;i<n;i++)
 	{
-		printf("\n\t   P%d\t\t   %d\t\t   %d\t\t   ",i,((pp_array+i)->burst_time),((pp_array+i)->arrival_time));
-		scanf("%d",&((pp_array+i)->priority));
+		do
+		{
+			printf("\t   P%d\t\t   %d\t\t   %d\t\t   ",i,((pp_array+i)->burst_time),((pp_array+i)->arrival_time));
+			scanf("%d",&((pp_array+i)->priority));
+			if((((pp_array+i)->priority)<1)||(((pp_array+i)->priority)>10))
+			printf("\t\t\t\t\t\t\tPRIORITY should be in range [1,10] !!Enter Again...\n");
+		}while((((pp_array+i)->priority)<1)||(((pp_array+i)->priority)>10));
 	}
 }
 //*****************************************************************************************************
 void display(struct processs *pp_array)
 {
-	system("clear");
+	system("cls");
+	printf("\nNumber of Processes : %d",n);
 	printf("\n\tPROCESS\t\tBURST_TIME\tARRIVAL_TIME\tPRIORITY\n");
 	for(i=0;i<n;i++)
 	printf("\n\t  P%d\t\t   %d\t\t   %d\t\t   %d",i,(pp_array+i)->burst_time,(pp_array+i)->arrival_time,(pp_array+i)->priority);
